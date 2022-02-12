e42b3a6ae361146f55bc4eec7e3663d8fe16e7bb
kou2911 <pnonkiti@icloud.com>
2022-02-12 21:35:11 +0900

    修正

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -39,7 +39,7 @@ import Vue from 'vue/dist/vue.esm'
    const app = new Vue({
      el: '#hello',
      data: {
-       message: "Can you say hello?"
+       message: "Can you "
      },
      components: { App }
    })
