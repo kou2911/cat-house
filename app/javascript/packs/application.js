8066931ccc1035abb6a8fd66ef164911ae248a8d
kou2911 <pnonkiti@icloud.com>
2022-02-21 21:38:07 +0900

    スライドショー導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require('jquery')
 require("../show")
 require("../preview")
+require("../index")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
